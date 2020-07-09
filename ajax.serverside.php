0b20255122c4c7dcbd38bea5b6a9932ed4be02dc
FatihGoktas <fthgkts@gmail.com>
2020-07-09 17:36:00 +0300

    Create ajax.serverside.php
    first look of serverside codes

---
+++ ajax.serverside.php
@@ -0,0 +1,64 @@
+<?php
+
+// SERVER-SIDE PROCESSING OF BOOTGRID
+
+/* CLIENT SIDE DATA */
+$params = $_POST; // OR $_GET
+
+/* INIT VARIABLES */
+$rows = [];
+$total = 0;
+$search = "";
+$order = "";
+$limit = "";
+
+/* SORT, ALWAYS USING (COLUMN, INIT SORT ETC.) */
+$sort['col'] = "ID";
+$sort['way'] = "DESC";
+if (isset($params['sort'])) {
+    foreach ($params['sort'] as $index => $item) {
+        $sort['col'] = $index;
+        $sort['way'] = $item;
+    }
+
+    $order = " ORDER BY {$sort['col']} {$sort['way']} ";
+}
+
+/* LIMIT, ALWAYS USING (PAGINATION ETC.) */
+if ($params['rowCount'] > -1) {
+    $offset = 0;
+    $row = 0;
+    if ($params['current'] == 1) {
+        $row = $params['rowCount'];
+    } else {
+        $row = $params['rowCount'];
+        $offset = ($params['rowCount'] * ($params['current']-1));
+    }
+    $limit = " OFFSET {$offset} ROWS FETCH NEXT {$row} ROWS ONLY ";
+}
+
+/* IF SEARCH USING */
+if (isset($params['searchPhrase']) && $params['searchPhrase'] != "") {
+    $search = " AND (col_1 LIKE '%{$params['searchPhrase']}%' OR col_2 LIKE '%{$params['searchPhrase']}%') ";
+}
+
+/* MAIN QUERY */
+$sql_group_users = "SELECT * FROM table WHERE 1 = 1 {$search} {$order} {$limit}";
+
+/* FETCH QUERY AS ARRAY DATA */
+$rows = $db->query($sql_group_users)->fetchAll();
+
+/* CALCULATING TOTAL ROWS*/
+if ($search != "") {
+    $total = count($db->query("SELECT * FROM table WHERE 1 = 1 {$search}")->fetchAll());
+} else {
+    $total = count($db->query("SELECT * FROM dbo.RB_BUTCE_GRUP_USER WHERE 1 = 1")->fetchAll());
+}
+
+/* PREPAIRING RESULTS */
+$_POST['rows'] = $rows;
+$_POST['total'] = $total;
+
+/* OUTPUT  */
+echo json_encode($_POST);
+exit();
